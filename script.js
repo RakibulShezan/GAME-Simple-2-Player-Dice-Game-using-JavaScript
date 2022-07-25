9ffb9e99643fed5d57ab561607adbd8046d49dc9
RakibulShezan <[rakibulshezan97@gmail.com]>
2022-07-25 13:41:08 +0600

    Rolling the Dice and Switiching the active players

--- script.js
+++ script.js
@@ -1,1 +1,50 @@
 'use strict';
+
+// Selecting Elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+
+const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnHold = document.querySelector('.btn--hold');
+const btnRoll = document.querySelector('.btn--roll');
+
+// Initial Look of the page
+score0El.textContent = 0;
+score1El.textContent = 0;
+
+diceEl.classList.add('hidden');
+
+const scores = [0, 0];
+let currentScore = 0;
+let activePlayer = 0;
+
+// Rolling the Dice
+btnRoll.addEventListener('click', function () {
+  // Generate a random dice value between 1 to 6
+  const dice = Math.trunc(Math.random() * 6) + 1;
+
+  //   Display the dice image matching the random generated dice value
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${dice}.png`;
+
+  //  Check If the dice is rolled to 1
+  if (dice !== 1) {
+    // Add dice to current score
+    currentScore += dice;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
+  } else {
+    // switch the current player to the oposition
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+
+    // Switch the background color on player switch
+    player0El.classList.toggle('player--active');
+    player1El.classList.toggle('player--active');
+  }
+});
